1e70b82548e0b1cbb1e818e779b88ec3e60954d9
Jonathan Mullen <jtmulle2@illinois.edu>
2017-10-22 18:34:59 -0500

    Combined exceptions into idt_init. Still have a definition error for stack segment fault

--- student-distrib/idt_init.c
+++ student-distrib/idt_init.c
@@ -1,5 +1,7 @@
 //include defines etc.
 #include "x86_desc.h"
+#include "idt_init.h"
+#include "lib.h"
 
 int i, addr; // loop variable
 char exceptions[20];
@@ -10,7 +12,7 @@ void idt_init() {
 
 		if(i != 15){
 		//set seg_selector bits
-		idt[i].seg_selector = gdt_ptr; //gedt_desc_ptr
+		idt[i].seg_selector = gdt_desc_ptr; //gedt_desc_ptr
 		//set reserved bits
 		idt[i].reserved0 = 0;
 		idt[i].reserved1 = 1;
@@ -38,7 +40,7 @@ void idt_init() {
 		SET_IDT_ENTRY(idt[9], &coprocessor_segment_overrun);
 		SET_IDT_ENTRY(idt[10], &invalid_tss);
 		SET_IDT_ENTRY(idt[11], &segment_not_present);
-		SET_IDT_ENTRY(idt[12], &stack_segment_fault);
+		SET_IDT_ENTRY(idt[12], &stack_segment);
 		SET_IDT_ENTRY(idt[13], &general_protection);
 		SET_IDT_ENTRY(idt[14], &page_fault);
 		SET_IDT_ENTRY(idt[16], &fpu_floating_point_exception);
@@ -50,5 +52,131 @@ void idt_init() {
 	lidt (idt_desc_ptr);
 
 	return;
+
+}
+
+
+
+
+void divide_by_zero(){
+	printf("divide_by_zero");
+	while(1);
+
+}
+
+//vector # 1 reserved for Intel use
+void debug(){
+	printf("debug exception");
+	while(1);
+
+}
+
+void nmi_interrupt(){
+	printf("nmi_interrupt");
+	while(1);
+
+}
+
+void breakpoint(){
+	printf("breakpoint");
+	while(1);
+
+}
+
+void overflow(){
+	printf("divide_by_zero");
+	while(1);
+
+}
+
+
+void bound_range_exceeded(){
+	printf("bound_range_exceeded");
+	while(1);
+
+}
+
+
+void invalid_opcode(){
+	printf("invalid_opcode");
+	while(1);
+
+}
+
+
+void device_not_available(){
+	printf("device_not_available");
+	while(1);
+
+}
+
+void double_fault(){
+	printf("double_fault");
+	while(1);
+
+}
+
+
+void coprocessor_segment_overrun(){
+	printf("coprocessor_segment_overrun");
+	while(1);
+
+}
+
+
+void invalid_tss(){
+	printf("invalid_tss");
+	while(1);
+
+}
+
+void segment_not_present(){
+	printf("segment_not_present");
+	while(1);
+
+}
+
+void stack_segment(){
+	printf("stack_segment_fault");
+	while(1);
+
+}
+
+void general_protection(){
+	printf("general_protection");
+	while(1);
+
+}
+
+void page_fault(){
+	printf("page_fault");
+	while(1);
+
+}
+
+//vector 15 reserved for Intel use
+
+void fpu_floating_point_exception(){
+	printf("floating_point_exception");
+	while(1);
+
+}
+
+
+void alignment_check(){
+	printf("alignment_check");
+	while(1);
+
+}
+
+void machine_check(){
+	printf("machine_check");
+	while(1);
+
+}
+
+void simd_floating_point_exception(){
+	printf("divide_by_zero");
+	while(1);
 
 }
--- student-distrib/idt_init.h
+++ student-distrib/idt_init.h
@@ -1,7 +1,28 @@
 #ifndef IDT_INIT
 #define IDT_INIT
 
+#ifndef ASM
 extern void idt_init();
 
+void divide_by_zero();
+void debug();
+void nmi_interrupt();
+void breakpoint();
+void overflow();
+void bound_range_exceeded();
+void invalid_opcode();
+void device_not_available();
+void double_fault();
+void coprocessor_segment_overrun();
+void invalid_tss();
+void segment_not_present();
+void stack_segement();
+void general_protection();
+void page_fault();
+void fpu_floating_point_exception();
+void alignment_check();
+void machine_check();
+void simd_floating_point_exception();
 
 #endif
+#endif /* IDT_INIT */ 
