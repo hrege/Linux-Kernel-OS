4eb9acfd0a52c8a85483aef2b37a83aff8a422f4
Austin Born <austinborn212@gmail.com>
2017-10-23 17:09:27 -0500

    added interfacing

---
+++ student-distrib/paging.h
@@ -0,0 +1,13 @@
+#ifndef PAGING_H
+#define PAGING_H
+
+#include "types.h"
+
+// Paging Initialization functions
+/*
+void paging_init();
+void paging_enable(uint32_t* pdir_addr);
+*/
+#endif /* PAGING_H */
+
+
