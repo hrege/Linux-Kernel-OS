6fb0920e22b0cf573ec03e5dccc65a70c00d4a02
hrege <davereg2@illinois.edu>
2017-10-26 00:27:51 -0500

    Keyboard working!

--- student-distrib/idt_init.c
+++ student-distrib/idt_init.c
@@ -4,7 +4,8 @@
 #include "lib.h"
 #include "types.h"
 #include "i8259.h"
- 
+#include "keyboard.h"
+
  int i, addr; // loop variable
 /*
 *	idt_init
@@ -20,7 +21,7 @@
 void idt_init() {
 	/*Initialize all IDT entries to default unused*/
 	for(i = 0; i < 256; i++){
-		idt[i].seg_selector = KERNEL_CS; 
+		idt[i].seg_selector = KERNEL_CS;
 		//set reserved bits
 		idt[i].reserved0 = 0;
 		idt[i].reserved1 = 0;
@@ -336,7 +337,7 @@ void sys_call(){
 
 void get_char(){
 	__asm__("pusha\n\t"
-			"call get_char_hlp\n\t"
+			"call keyboard_handler\n\t"
 			"popa\n\t"
 			"leave\n\t"
 			"IRET\n\t");
@@ -490,19 +491,6 @@ void simd_floating_point_exception_hlp(){
 void sys_call_hlp(){
 	printf("This is a system call\n");
 	while(1);
-}
-
-void get_char_hlp(){
-  unsigned char keyboard_status;
-  char keyboard_input;
-
-  keyboard_status = inb(0x64);
-  if((keyboard_status & 0x01) != 0) {
-    keyboard_input = inb(0x60);
-    //use scancode mapping
-	}
-
-  send_eoi(1);
 }
 
 void rtc_int_hlp(){
