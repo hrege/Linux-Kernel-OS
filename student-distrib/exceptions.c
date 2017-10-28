7a68901df0c1369651709b4c336df4ce2a1122fd
Jonathan Mullen <jtmulle2@illinois.edu>
2017-10-27 22:47:10 -0500

    Here are the actual new files for exception handling

---
+++ student-distrib/exceptions.c
@@ -0,0 +1,132 @@
+/*
+*	C Exception Handlers. 
+*	These handlers are linked to from the idt through assembly linkage in exception_link.S
+*	Handlers originally created by Sam
+*	Reorganized and put in separate file by Jonathan
+*	If/when handlers are more filled out check funtion interfaces for authors
+*	At this point functions only print the name of the exception so one template interface is
+*	provided at the top for divide by zero exception
+*
+*			VictoriOS Secret
+*
+*/
+
+#include "exceptions.h"
+#include "lib.h"
+#include "types.h"
+
+
+/*
+*	divide_by_zero
+*		Description: C Handler for the divide by zero exception
+*						-Current doesn't do much
+*		Input: none
+*		Output: none
+*		Return: Doesn't
+*		Side-effects: Prints the name of the exception reached
+*
+*/
+
+void divide_by_zero() {
+	printf("divide_by_zero\n");
+	while(1);
+}
+
+//vector # 1 reserved for Intel use
+void debug(){
+	printf("debug exception\n");
+	while(1);
+}
+
+void nmi_interrupt(){
+	printf("nmi_interrupt\n");
+	while(1);
+}
+
+void breakpoint(){
+	printf("breakpoint\n");
+	while(1);
+}
+
+void overflow(){
+	printf("divide_by_zero\n");
+	while(1);
+}
+
+void bound_range_exceeded(){
+	printf("bound_range_exceeded\n");
+	while(1);
+}
+
+void invalid_opcode(){
+	printf("invalid_opcode\n");
+	while(1);
+}
+
+
+void device_not_available(){
+	printf("device_not_available\n");
+	while(1);
+}
+
+void double_fault(){
+	printf("this is a double_fault\n");
+	while(1);
+}
+
+void coprocessor_segment_overrun(){
+	printf("coprocessor_segment_overrun\n");
+	while(1);
+}
+
+void invalid_tss(){
+	printf("invalid_tss\n");
+	while(1);
+}
+
+void segment_not_present(){
+	printf("segment_not_present\n");
+	while(1);
+}
+
+void stack_segment(){
+	printf("stack_segment_fault\n");
+	while(1);
+}
+
+void general_protection(){
+	printf("general_protection\n");
+	while(1);
+
+}
+
+void page_fault(){
+	printf("page_fault\n");
+	while(1);
+}
+
+//vector 15 reserved for Intel use
+void assertion_fail(){
+	printf("Assertion failure\n");
+	while(1);
+}
+
+void fpu_floating_point_exception(){
+	printf("floating_point_exception\n");
+	while(1);
+}
+
+void alignment_check(){
+	printf("alignment_check\n");
+	while(1);
+}
+
+void machine_check(){
+	printf("machine_check\n");
+	while(1);
+}
+
+void simd_floating_point_exception(){
+	printf("divide_by_zero\n");
+	while(1);
+}
---
+++ student-distrib/exceptions.h
@@ -0,0 +1,78 @@
+/*
+*  Author: Jonathan - Group 31 victoriOS Secret
+*
+*
+*	exceptions file is for the exception handlers
+*/
+
+#ifndef _EXCEPTIONS_H
+#define _EXCEPTIONS_H
+
+#include "types.h"
+
+#ifndef ASM
+/* divide by zero handler */
+void divide_by_zero();
+
+//vector # 1 reserved for Intel debug use
+extern void debug();
+
+//non-maskable interrupt exception handler
+extern void nmi_interrupt();
+
+//breakpoint exception handler
+extern void breakpoint();
+
+//overflow exception handler
+extern void overflow();
+
+//range exceeded exception handler
+extern void bound_range_exceeded();
+
+//invalid opcode exception handler
+extern void invalid_opcode();
+
+//unavailable device exception handler
+extern void device_not_available();
+
+//double fault exception handler
+extern void double_fault();
+
+//segment overrun exception handler
+extern void coprocessor_segment_overrun();
+
+//invalid tss exception handler
+extern void invalid_tss();
+
+//segment not pressed exception handler
+extern void segment_not_present();
+
+//stack segment exception handler
+extern void stack_segment();
+
+//general protection exception handler
+extern void general_protection();
+
+//page fault exception handler
+extern void page_fault();
+
+//vector 15 reserved for Intel use
+//we use to handle assertion fail exception handler
+extern void assertion_fail();
+
+// /fpu floating point exception handler
+extern void fpu_floating_point_exception();
+
+//alignment exception handler
+extern void alignment_check();
+
+//machine check exception handler
+extern void machine_check();
+
+//simd floating point exception handler
+extern void simd_floating_point_exception();
+
+
+#endif
+#endif
+
