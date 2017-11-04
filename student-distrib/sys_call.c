ba65bf09b3954893fc61940fe86a7e4f84179ab9
Jonathan Mullen <jtmulle2@illinois.edu>
2017-11-04 10:48:18 -0500

    Just some more outline

--- student-distrib/sys_call.c
+++ student-distrib/sys_call.c
@@ -14,7 +14,14 @@ extern int32_t sys_halt(uint8_t status){
 
 }
 extern int32_t sys_execute(const uint8_t* command){
-
+/*
+	-Parse
+	-Check if an executable
+	-Set up Paging
+	-User level program loader
+	-Creae PCB for the program
+	-COntext switch
+/*
 
 }
 extern int32_t sys_read(int32_t fd, void* buf, int32_t nbytes){
@@ -30,12 +37,21 @@ extern int32_t sys_open(const uint8_t* filename){
 	/*Select fd to be first available in fd_array located in the PCB struct*/
 	/*Switch by file type*/
 	/*associate correct ops in fd_array[fd].ops_table*/
+	/* Do the open function for that file? */
+
+	get_first_fd() //should get the first available fd and returns its index... -1 if full
+	
+
 
 }
 extern int32_t sys_close(int32_t fd){
 
 
 }
+
+
+/*    BELOW NOT FOR CP3   */
+
 extern int32_t sys_getargs(uint8_t* buf, int32_t nbytes){
 
 
