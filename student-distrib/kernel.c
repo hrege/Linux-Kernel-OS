efd14e20e700b78b5fdf7b44d5fe517eafaa3647
Jonathan Mullen <jtmulle2@illinois.edu>
2017-11-15 22:41:22 -0600

    Fixed sys_call linkage register saving & restoration

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -178,7 +178,7 @@ void entry(unsigned long magic, unsigned long addr) {
 #endif
     /* Execute the first program ("shell") ... */
      //uint8_t* ptr = (uint8_t*)("shell");
-    uint8_t* ptr = (uint8_t*)("testprint");
+    uint8_t* ptr = (uint8_t*)("shell");
     sys_execute(ptr);
 
     /* Spin (nicely, so we don't chew up cycles) *///
