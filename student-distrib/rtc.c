20f90b3b9761f000def98c31206ccb605da7a533
Jonathan Mullen <jtmulle2@illinois.edu>
2017-12-10 23:22:22 -0600

    RTC Virtualized!

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -11,7 +11,21 @@
 #include "x86_desc.h"
 
 //filescope variables:
-int occurred = 0;  //indicator that the interrupt occurred for read
+
+
+//rates for each terminal
+int rate[] = {0,0,0};
+//inverse power rate for each (see rtc write for math)
+int8_t inverse[] = {0,0,0};
+//rate it is set at
+int set_rate = 0; 
+//inverse i used to set
+int8_t set_i = 0;
+//counter for each terminal
+uint32_t count[] = {0,0,0};
+//counter target value for each
+uint32_t target[] = {0,0,0}; 
+
 
 /* rtc_init
 *   Description: Initialize RTC clock interrupt.
@@ -36,9 +50,9 @@ void rtc_init() {
   char prev = inb(RTC_PORT_CMOS);
   outb(RTC_B_REG, RTC_PORT);
   outb((prev | BIT_SIX_ENABLE), RTC_PORT_CMOS);
-
-  /* Enable IRQ line on Slave PIC for RTC. */
+  /* Enable IRQ line for RTC. */
   enable_irq(RTC_PIC_IRQ);
+
 }
 
 /*
@@ -62,8 +76,13 @@ void rtc_handler() {
   /* Send EOI signals to both Slave PIC and Master PIC. */
   send_eoi(RTC_PIC_IRQ);
   send_eoi(SLAVE_IRQ);
-  //mark that the interrupt happened
-  occurred = 1;
+  //increment counters
+  int i;
+  for(i=0; i<NUM_TERMS; i++){
+  	count[i]++;
+  }
+
+  return;
 }
 
 
@@ -84,8 +103,10 @@ void rtc_handler() {
 *		Side Effects: RTC running at 2Hz
 */
 int32_t rtc_open(const uint8_t* filename){
+
 	int freq = 2;
 	rtc_write(0, &freq, 0);  //call rtc_write with freq of 2Hz
+	rate[active_term] = freq;
 	return 0;
 }
 
@@ -99,11 +120,11 @@ int32_t rtc_open(const uint8_t* filename){
 *		Returns: 0 (always)
 *		Side effects: blocks
 */
-int32_t rtc_read(int32_t fd, void* buf, int32_t nbytes){
-	occurred = 0;  
+int32_t rtc_read(int32_t fd, void* buf, int32_t nbytes){ 
 	sti();
-	while(!occurred);  //wait for interrupt to occur
+	while(count[active_term]<target[active_term]);  //wait for interrupt to occur
 	cli();
+	count[active_term] = 0;
 	return 0;
 
 }
@@ -126,20 +147,28 @@ int32_t rtc_write(int32_t fd, const void* buf, int32_t nbytes){
 		return -1;
 	}
 	int32_t frequency = *((uint32_t *)buf);
-	if(frequency >= MIN_FREQ && frequency <= MAX_FREQ){
-		int32_t f = 2; //frequency to check against
+	if((frequency >= MIN_FREQ) && frequency <= MAX_FREQ){
+		int f = 2; //frequency to check against
 		int8_t i = 14;	   //inverse power counter (minus 1)
 		do{
+			//if called with 
 			if(frequency == f){
-				/*if valid frequency then write to RTC
+				/*if valid frequency input then write maximum
+				* frequency to rtc if not already at that rate
 				* write to A register of CMOS bottom 4
 				* value is !(power-1)  */
-				cli();
-				outb(RTC_A_REG, RTC_PORT);		//Select A Register
-				char prev = inb(RTC_PORT_CMOS); //get old to keep top 4
-				outb(RTC_A_REG, RTC_PORT);
-				outb((prev & 0xF0)|(i & 0x0F), RTC_PORT_CMOS); //keep same upper 4 put freq value into lower 4
-				sti();
+				rate[active_term] = f;
+				inverse[active_term] = i;
+
+				if(f > set_rate){
+					rate_increase(f);
+					rtc_output(i);
+				}
+				else{
+					//initialize counter and target
+					count[active_term] = 0;
+					target[active_term] = set_rate / f;
+				}
 				return 0; //return success
 			}
 			f = f*2;
@@ -149,6 +178,70 @@ int32_t rtc_write(int32_t fd, const void* buf, int32_t nbytes){
 	return -1; //return fail
 }
 
+/* rtc_output
+*		Author: Jonathan
+*		Description: does the output to the rtc
+*		Inputs:  inverse power for setting corresponding to rate
+*		Outputs: to RTC ports
+*		Return: none
+*		Side-effect: RTC rate changed
+*/
+void rtc_output(int8_t i){
+	set_i = i;
+	cli();//to be safe
+	outb(RTC_A_REG, RTC_PORT);		//Select A Register
+	char prev = inb(RTC_PORT_CMOS); //get old to keep top 4
+	outb(RTC_A_REG, RTC_PORT);
+	outb((prev & 0xF0)|(i & 0x0F), RTC_PORT_CMOS); //keep same upper 4 put freq value into lower 4
+	//sti();
+	return;
+}
+
+/* rate_increase
+*		Author: Jonathan
+*		Description: updates the rate and the counter values to match if new_rate is larger (from write)
+*		Inputs: rate switching to
+*		Outputs: none
+*		Return: none
+*		Side-effect: RTC counters & targets adjusted
+*/
+void rate_increase(int new_rate){
+	int i;
+	//case where all closed
+	if(set_rate != 0){
+		int mult = new_rate / set_rate;  
+		for(i = 0; i<NUM_TERMS; i++){
+			if(i!=active_term){
+				count[i] *= mult;
+				target[i] *= mult;
+			}
+		}
+	}
+	count[active_term] = 0;
+	target[active_term] = 1;
+	set_rate = new_rate;
+	return;
+}
+/* rate_decrease
+*		Author: Jonathan
+*		Description: updates the rate and the counter values to match when new_rate is smaller (from close)
+*		Inputs: rate switching to
+*		Outputs: none
+*		Return: none
+*		Side-effect: RTC counters & targets adjusted
+*/
+void rate_decrease(int new_rate){
+	int i;
+	int div = set_rate / new_rate;
+	for(i = 0; i<NUM_TERMS; i++){
+		if(i!=active_term){
+			count[i] /= div;
+			target[i] /= div;
+		}
+	}
+	set_rate = new_rate;
+	return;
+}
 /*
 *	rtc_close
 *		Author: Jonathan
@@ -160,9 +253,30 @@ int32_t rtc_write(int32_t fd, const void* buf, int32_t nbytes){
 *		Side effects: none
 */
 int32_t rtc_close(int32_t fd){
+	rate[active_term] = 0;
+	inverse[active_term] = 0;
+	int max_rate = 0;
+	int8_t i = 0;
+	int j;
+	for(j=0; j<NUM_TERMS; j++){
+		if(rate[j]>max_rate){
+			max_rate = rate[j];
+			i = inverse[j];
+		}
+	}
 	
-  PCB_t* curr_pcb = (PCB_t*)((int32_t)tss.esp0 & 0xFFFFE000);
+	//if all closed set rtc to slowest rate to reduce interrupts
+	if(max_rate == 0){
+		int freq = 2;
+		rtc_write(0, &freq, 0);  //call rtc_write with freq of 2Hz
+	}
+	//if the rate is changing
+	else if(max_rate != set_rate){
+		rate_decrease(max_rate);
+		rtc_output(i);
+	}
 
+  PCB_t* curr_pcb = get_pcb();
   curr_pcb->file_array[fd].flags = 0;
   curr_pcb->file_array[fd].file_position = 0;
 
